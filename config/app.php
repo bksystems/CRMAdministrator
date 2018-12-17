fb837992410d811ae0725bc2ec139dfb2e1137d7
Juan Rizo <jcarlos.rizo@gmail.com>
2018-12-17 08:30:51 -0600

    Load db

--- config/app.php
+++ config/app.php
@@ -257,7 +257,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => '',
-            'database' => 'genesis_app_db',
+            'database' => 'CRMAdministrator',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
